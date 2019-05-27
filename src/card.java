1d2056720b841cbdc6dfd5f52acb1f8034f8313a
Sapnavishnoi <sapna18@navgurukul.org>
2019-05-27 11:59:24 +0530

    add card class

---
+++ src/card.java
@@ -0,0 +1,9 @@
+public class card {
+    String question;
+    String answer;
+
+    public card(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+}
